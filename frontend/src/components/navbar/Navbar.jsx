6f64ab83d5b7e3d3cd7b28efdd8d5453221bcaf2
Praduam Prakash <praduam18kash@gmail.com>
2026-08-17 02:57:31 +0530

    created navbar and create all the possible routes

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Navbar = ({className}) => {
+  return (
+   <>
+   <div className="nav-container">
+    
+   </div>
+   </>
+  )
+}
---
+++ frontend/src/pages/Homepage/MainHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const MainHome = () => {
+
+
+    
+  return (
+    <>
+    <div className='parent-container'> 
+       
+    </div>
+    </>
+    
+  )
+}
