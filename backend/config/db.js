730101f33bcf470fcd38412504ff8ddca0380224
Praduam Prakash <praduam18kash@gmail.com>
2026-09-17 23:18:53 +0530

    fix: Vercel error

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
-    ssl: process.env.DB_HOST.includes('aivencloud.com') ? { rejectUnauthorized: false } : undefined,
+    ssl: (process.env.DB_HOST && process.env.DB_HOST.includes)('aivencloud.com') ? { rejectUnauthorized: false } : undefined,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
